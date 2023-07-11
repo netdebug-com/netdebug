a4a432e8edb37f7753a2c896426dd2dae085b502
Rob Sherwood <rob.sherwood@gmail.com>
2023-07-11 01:54:29 -0700

    Pathetic shell script!

---
+++ update_and_run_prod.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -x
+
+git pull && \
+	cargo b --release && \
+	wasm-pack build --target=web webserver/web-client/ && \
+	sudo ./target/release/webserver --production --listen-port 443 \
+	|& tee out.$$.log
