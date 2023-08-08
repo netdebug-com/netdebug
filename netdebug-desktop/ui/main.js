0a60367ea474ae36afc67c68ae2ffd8f48cb31cc
Rob Sherwood <rob.sherwood@gmail.com>
2023-08-08 08:25:02 -0700

    Somewhat working/broken tabs and geolocation
    need to better understand windows/iframe dynamics or architect
    a scheme for postMessage back to the parent to get the info.

--- netdebug-desktop/ui/main.js
+++ netdebug-desktop/ui/main.js
@@ -1,61 +1,27 @@
-const { invoke } = window.__TAURI__.tauri
 
-var interval = null
+function loadtab (element) {
+  var container = document.getElementById('container')
+  console.log('loadTab - ', element.rel)
+  container.src = element.rel
 
-// with double-buffering fakery
-async function update_table (table) {
-  let flows = await invoke('dump_connection_keys')
-  let keys = Object.keys(flows)
-  console.log("Flows = ", flows, "keys=", keys)
-  var table1 = document.querySelector('#keys-table1')
-  var table2 = document.querySelector('#keys-table2')
-  var old_table_body
-  // only update the hidden table
-  if (table1.style.display == 'none') {
-    old_table_body = document.querySelector('#keys-table-body1')
-  } else {
-    old_table_body = document.querySelector('#keys-table-body2')
-  }
-  old_table_body.innerHTML = ''
-  if (keys.length == 0) {
-    var row = old_table_body.insertRow()
-    row.insertCell().textContent = count
-    row.insertCell().textContent = 'No flows -- really!?'
-  } else {
-    var count = 0
-    keys.forEach(k => {
-      var row = old_table_body.insertRow()
-      count = count + 1
-      row.insertCell().textContent = count
-      row.insertCell().textContent = k
-    })
-  }
-  if (table1.style.display == 'none') {
-    table1.style.display = 'inline'
-    table2.style.display = 'none'
-  } else {
-    table1.style.display = 'none'
-    table2.style.display = 'inline'
+  var tabs = document.getElementById('tabs').getElementsByTagName('a')
+  for (var i = 0; i < tabs.length; i++) {
+    if (tabs[i].rel == element.rel) tabs[i].className = 'selected'
+    else tabs[i].className = ''
   }
 }
 
-function toggleInterval () {
-  var button = document.querySelector('#pause-button')
-  if (interval == null) {
-    interval = window.setInterval(update_table, 500)
-    console.log('Enabling updates: new interval=', interval)
-    button.textContent = 'Pause Updates'
-  } else {
-    clearInterval(interval)
-    interval = null
-    console.log('Pausing updates')
-    button.textContent = 'Enable Updates'
+function initTabs () {
+  var tabs = document.getElementById('tabs').getElementsByTagName('a')
+  var container = document.getElementById('container')
+  container.src = tabs[0].rel
+  // force the onClick attribute here to avoid HTML loading time bugs?!
+  for (var i = 0; i < tabs.length; i++) {
+      tabs[i].addEventListener('click', (e) => {
+        loadtab(e.target)
+      })
   }
+  console.log('initTabs(): done')
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-  toggleInterval()
-  document
-    .getElementById('pause-button')
-    .addEventListener('click', toggleInterval)
-})
+window.onload = initTabs
