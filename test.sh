646b9c51362ca0e1f9fde3202291a801d2e16414
Rob Sherwood <rob.sherwood@gmail.com>
2023-08-09 04:40:31 -0700

    Added web-gui to test script

--- test.sh
+++ test.sh
@@ -4,3 +4,4 @@ set -x
 cargo t $@
 # WASM integration tests are not run through cargo - sigh
 wasm-pack test --chrome --headless $@ webserver/web-client
+wasm-pack test --chrome --headless $@ desktop/web-gui
