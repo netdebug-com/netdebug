86a8fb2f8ce9424e8fe6e602e2c99439286ebbd6
Rob Sherwood <rob.sherwood@gmail.com>
2023-08-09 07:59:03 -0700

    html! macro!  should dramamtically simplify things
    Look - I can make a macro - it only took me an hour!

--- desktop/web-gui/src/utils.rs
+++ desktop/web-gui/src/utils.rs
@@ -8,3 +8,31 @@ pub fn set_panic_hook() {
     #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
 }
+
+/**
+ * Super helpful HTML macro that will create an element and set all of the attributes
+ *
+ * /// let div = html!("div", {
+ * ///  "name" => "root_div",
+ * ///  "id" => "container",
+ * ///   /* .... more attributes*/
+ * /// })
+ * /// assert_eq!(div.get_attribute("name"), Some("root_div");
+ * ///
+ */
+
+#[macro_export]
+macro_rules! html {
+    ($e:expr, {$( $k:expr => $v:expr ),* $(,)?}) => {
+        {
+        (|| -> Result<web_sys::Element, wasm_bindgen::JsValue> {
+        let d = web_sys::window().expect("window").document().expect("document");
+        let element = d.create_element($e)?;
+        $(
+            element.set_attribute($k,$v)?;
+        )*
+        Ok(element)
+    })()
+    }
+    }
+}
