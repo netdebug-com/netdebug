8d79ece803fb810e27411343d45cef01feef4698
Rob Sherwood <135524607+capveg-netdebug@users.noreply.github.com>
2023-09-06 15:31:05 -0700

    Fixed the unused warnings in build.rs (#10)
    Under Linux and MacOS, these print 'unused' warnings because
    they only do stuff in windows

--- desktop/build.rs
+++ desktop/build.rs
@@ -1,11 +1,14 @@
 use std::env;
+#[cfg(windows)]
 use std::path::Path;
 
 fn main() {
-    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     #[cfg(windows)]
-    println!(
-        "cargo:rustc-link-search=native={}",
-        Path::new(&dir).join("../lib/x64").display()
-    );
+    {
+        let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+        println!(
+            "cargo:rustc-link-search=native={}",
+            Path::new(&dir).join("../lib/x64").display()
+        );
+    }
 }
