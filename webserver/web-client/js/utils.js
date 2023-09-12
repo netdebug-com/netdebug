e8e5b8f1cdb5b687aed44e9d4150fdbaa709021c
Gregor Maier <143829239+gregor-netdebug@users.noreply.github.com>
2023-09-12 01:04:22 -0400

    Autoformat all files that use prettier. (#30)
    Summary:
    `prettier .`
    
    Test Plan:

--- webserver/web-client/js/utils.js
+++ webserver/web-client/js/utils.js
@@ -5,36 +5,43 @@
 export function plot_chart(chart, cfg, verbose) {
     try {
         if (verbose) {
-            console.log("Creating Chart on canvas " + chart + " :: " + JSON.stringify(cfg, undefined, 2))
+            console.log(
+                "Creating Chart on canvas " +
+                    chart +
+                    " :: " +
+                    JSON.stringify(cfg, undefined, 2),
+            );
         }
         const ctx = document.getElementById(chart);
-        return new Chart(ctx, cfg );
-    }
-    catch(err) {
-        console.error("Error creating Chart() with cfg=" + cfg + " :: " + err)
+        return new Chart(ctx, cfg);
+    } catch (err) {
+        console.error("Error creating Chart() with cfg=" + cfg + " :: " + err);
     }
 }
 
 export function plot_json_chart(chart, json, verbose) {
-    let cfg = JSON.parse(json)
-    return plot_chart(chart, cfg, verbose)
+    let cfg = JSON.parse(json);
+    return plot_chart(chart, cfg, verbose);
 }
 
 export function plot_json_chart_update(chart, data_json, verbose) {
-    let data = JSON.parse(data_json)
+    let data = JSON.parse(data_json);
     if (verbose) {
-        console.log("Setting new data to :: " + JSON.stringify(data, undefined, 2))
+        console.log(
+            "Setting new data to :: " + JSON.stringify(data, undefined, 2),
+        );
     }
     // clear out the old data
     chart.data.datasets.forEach((dataset) => {
         dataset.data = [];
     });
     // this is where I'd love to have some type checking
-    chart.data.datasets = data
-    chart.update('none')     // 'none' says don't re-annimate with each update
+    chart.data.datasets = data;
+    chart.update("none"); // 'none' says don't re-annimate with each update
 }
 
-export function plot_latency_chart(chart, 
+export function plot_latency_chart(
+    chart,
     best_isp,
     best_home,
     best_app,
@@ -44,51 +51,59 @@ export function plot_latency_chart(chart,
     worst_isp,
     worst_home,
     worst_app,
-    verbose) {
-
+    verbose,
+) {
     const data = [
-        {x: "Best-Case", isp: best_isp, home: best_home, app: best_app},
-        {x: "Typical-Case", isp: typical_isp, home: typical_home, app: typical_app},
-        {x: "Worst-Case", isp: worst_isp, home: worst_home, app: worst_app},
-
+        { x: "Best-Case", isp: best_isp, home: best_home, app: best_app },
+        {
+            x: "Typical-Case",
+            isp: typical_isp,
+            home: typical_home,
+            app: typical_app,
+        },
+        { x: "Worst-Case", isp: worst_isp, home: worst_home, app: worst_app },
     ];
 
     const cfg = {
-        type: 'bar',
+        type: "bar",
         data: {
-            labels: ['Best-Case', 'Typical-Case', 'Worst-Case'],
-            datasets: [{
-                label: 'ISP latency (ms)',
-                data: data,
-                parsing: {
-                    yAxisKey: 'isp'
-                }
-            }, {
-                label: 'Home Network latency (ms)',
-                data: data,
-                parsing: {
-                    yAxisKey: 'home'
-                }
-            }, {
-                label: 'Application latency (ms)',
-                data: data,
-                parsing: {
-                    yAxisKey: 'app'
-                }
-            }]
+            labels: ["Best-Case", "Typical-Case", "Worst-Case"],
+            datasets: [
+                {
+                    label: "ISP latency (ms)",
+                    data: data,
+                    parsing: {
+                        yAxisKey: "isp",
+                    },
+                },
+                {
+                    label: "Home Network latency (ms)",
+                    data: data,
+                    parsing: {
+                        yAxisKey: "home",
+                    },
+                },
+                {
+                    label: "Application latency (ms)",
+                    data: data,
+                    parsing: {
+                        yAxisKey: "app",
+                    },
+                },
+            ],
         },
         options: {
             scales: {
                 x: {
-                    stacked: true
+                    stacked: true,
                 },
                 y: {
                     stacked: true,
-                    "title": {
-                        "display": true,
-                        "text": "RTT (milliseconds)"
-                    }
-                }
+                    title: {
+                        display: true,
+                        text: "RTT (milliseconds)",
+                    },
+                },
             },
         },
     };
