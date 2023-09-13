d2ed89c15838a30ee469c59b9f47c435bb966763
Rob Sherwood <rob.sherwood@netdebug.com>
2023-09-13 11:15:52 -0700

    Added 'last_packet_time' to `struct Connection` and measurement
    Summary:
    
    To help find stale connections that the connection manager shouldn't
    be holding on to and help prove/disprove my theory for why
    we can't map a bunch of connections back to their processes
    
    Test Plan:

--- libconntrack-wasm/src/connection_measurements.rs
+++ libconntrack-wasm/src/connection_measurements.rs
@@ -27,5 +27,6 @@ pub struct ConnectionMeasurements {
     pub user_agent: Option<String>, // when created via a web request, store the user-agent header
     pub associated_apps: HashMap<u32, Option<String>>, // PID --> ProcessName, if we know it
     // TODO: add local_syn, remote_syn IP and TCP options
-    pub start_tracking_time: DateTime<Utc>, // time tracker first saw a packet
+    pub start_tracking_time: DateTime<Utc>, // time tracker: first saw a packet
+    pub last_packet_time: DateTime<Utc>, // time tracker: last saw a packet
 }
