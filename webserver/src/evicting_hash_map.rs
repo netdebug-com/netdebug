39824a23c534aabec165fda7fcccb58b75f044fe
Gregor Maier <gregor.maier@netdebug.com>
2023-09-18 20:43:27 -0400

    Add EvictingHashMap -- an LRU hashmap
    Summary:
    as titled.
    Probably should not live in libconntrack in the long term but good enough for
    now
    
    Test Plan:
    added unit test.

---
+++ webserver/src/evicting_hash_map.rs
@@ -0,0 +1,137 @@
+use std::borrow::Borrow;
+use std::hash::Hash;
+
+use linked_hash_map::LinkedHashMap;
+
+pub struct EvictingHashMap<'a, K, V> {
+    // Fun with callbacks: https://stackoverflow.com/questions/41081240/idiomatic-callbacks-in-rust
+    map: LinkedHashMap<K, V>,
+    max_elements: usize,
+    cb: Box<dyn FnMut(K, V) + 'a>,
+}
+
+/// A hash-map with a limited max capacity. Once capacity is reached, adding
+/// additional entries to the map will evict the least-recently-used entry.
+/// LRU is determined by last *access*.
+impl<'a, K, V> EvictingHashMap<'a, K, V>
+where
+    K: Hash + Eq,
+{
+    /// Create a new map with maximum size `max_elements`
+    /// If an entry is evicted due to max size, `eviction_cb()` will be called with the
+    /// evicted key,value pair.
+    pub fn new(max_elements: usize, eviction_cb: impl FnMut(K, V) + 'a) -> Self {
+        EvictingHashMap {
+            map: LinkedHashMap::with_capacity(max_elements),
+            max_elements: max_elements,
+            cb: Box::new(eviction_cb),
+        }
+    }
+
+    /// Returns the keys
+    pub fn keys(&self) -> linked_hash_map::Keys<K, V> {
+        self.map.keys()
+    }
+
+    pub fn insert(&mut self, k: K, v: V) -> Option<V> {
+        while self.map.len() >= self.max_elements && !self.map.contains_key(&k) {
+            // evict oldest entry
+            let evicted = self.map.pop_front().unwrap();
+            (self.cb)(evicted.0, evicted.1);
+        }
+        self.map.insert(k, v)
+    }
+
+    /// Returns the value corresponding to the key in the map.
+    /// If value is found, it is moved to the most-recent position of the LRU
+    pub fn get_mut<Q: ?Sized>(&mut self, k: &Q) -> Option<&mut V>
+    where
+        K: Borrow<Q>,
+        Q: Eq + Hash,
+    {
+        self.map.get_refresh(k)
+    }
+
+    /// Returns the value corresponding to the key in the map. *WITHOUT* updating the
+    /// LRU.
+    pub fn get_no_lru<Q: ?Sized>(&self, k: &Q) -> Option<&V>
+    where
+        K: Borrow<Q>,
+        Q: Eq + Hash,
+    {
+        self.map.get(k)
+    }
+
+    /// Remove the value corresponding to the key. If value was in the map,
+    /// it will be returned.
+    pub fn remove<Q: ?Sized>(&mut self, k: &Q) -> Option<V>
+    where
+        K: Borrow<Q>,
+        Q: Eq + Hash,
+    {
+        self.map.remove(k)
+    }
+
+    pub fn len(&self) -> usize {
+        self.map.len()
+    }
+}
+
+#[cfg(test)]
+pub mod test {
+    use super::*;
+    use std::sync::mpsc::{self, TryRecvError};
+
+    fn get_keys(map: &EvictingHashMap<i32, String>) -> Vec<i32> {
+        map.keys().cloned().collect()
+    }
+
+    #[test]
+    pub fn test() {
+        let (tx, rx) = mpsc::sync_channel(1);
+        let cb = |k, v| {
+            tx.try_send((k, v)).expect("Failed to write to MSPC queue");
+        };
+        let mut m = EvictingHashMap::<i32, String>::new(5, cb);
+
+        m.insert(1, "a".to_string());
+        assert_eq!(rx.try_recv(), Err(TryRecvError::Empty));
+        assert_eq!(m.len(), 1);
+        m.insert(2, "b".to_string());
+        assert_eq!(rx.try_recv(), Err(TryRecvError::Empty));
+        m.insert(3, "c".to_string());
+        assert_eq!(rx.try_recv(), Err(TryRecvError::Empty));
+
+        assert_eq!(get_keys(&m), vec![1, 2, 3]);
+
+        // access element w/o LRU update
+        assert_eq!(m.get_no_lru(&1), Some(&"a".to_string()));
+        assert_eq!(get_keys(&m), vec![1, 2, 3]);
+
+        // bump access to 1
+        assert_eq!(m.get_mut(&1), Some(&mut "a".to_string()));
+        assert_eq!(get_keys(&m), vec![2, 3, 1]);
+
+        m.insert(4, "d".to_string());
+        assert_eq!(rx.try_recv(), Err(TryRecvError::Empty));
+        m.insert(5, "e".to_string());
+        assert_eq!(rx.try_recv(), Err(TryRecvError::Empty));
+        // We have reached capacity now.
+        assert_eq!(get_keys(&m), vec![2, 3, 1, 4, 5]);
+
+        m.insert(6, "f".to_string());
+        assert_eq!(rx.try_recv(), Ok((2, "b".to_string())));
+        assert_eq!(get_keys(&m), vec![3, 1, 4, 5, 6]);
+
+        // update a value
+        let val = m.get_mut(&4);
+        assert!(val.is_some());
+        *val.unwrap() = "foo".to_string();
+        assert_eq!(m.get_no_lru(&4), Some(&"foo".to_string()));
+        assert_eq!(get_keys(&m), vec![3, 1, 5, 6, 4]);
+
+        // removal
+        assert_eq!(m.remove(&1), Some("a".to_string()));
+        assert_eq!(get_keys(&m), vec![3, 5, 6, 4]);
+    }
+}
--- webserver/src/lib.rs
+++ webserver/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod context;
+pub mod evicting_hash_map;
 pub mod http_routes;
 pub mod webtest;
