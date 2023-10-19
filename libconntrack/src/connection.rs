2ef01b5e1baf3f2e5ec352d8929100267f6e55da
Gregor Maier <gregor.maier@netdebug.com>
2023-10-19 10:31:26 -0400

    ConnctionTracker: Only send connections with probes to the storage server
    Summary:
    as titled
    
    Test Plan:
    added a new unit test.

--- libconntrack/src/connection.rs
+++ libconntrack/src/connection.rs
@@ -187,6 +187,10 @@ fn send_connection_storage_msg(
     storage_service_msg_tx: &Option<mpsc::Sender<ConnectionStorageEntry>>,
     mut c: Connection,
 ) {
+    if c.probe_report_summary.raw_reports.is_empty() && !c.in_active_probe_session() {
+        // This connection has not probes. Don't bother exporting it
+        return;
+    }
     if let Some(tx) = storage_service_msg_tx.as_ref() {
         if let Err(e) = tx.try_send(c.to_connection_storage_entry()) {
             // TODO: Should probably rate-limit the potential log message at some
@@ -2187,6 +2191,76 @@ pub mod test {
         assert_eq!(evicted.local_ip, "192.168.1.238");
         assert_eq!(evicted.remote_ip, "34.121.150.27");
         assert_eq!(evicted.remote_port, 443);
+
+        // make sure we have the "unrelated" connection in the tracker
+        assert!(connection_tracker
+            .connections
+            .get_no_lru(&unrelated_conn_key)
+            .is_some());
+
+        tokio::time::resume();
+    }
+
+    /// Tests that when a connection gets evicted we only send it to the storage server
+    /// if it has probe information in it.
+    #[tokio::test]
+    async fn test_send_only_conns_with_probe_to_storage() {
+        let max_connections_per_tracker = 32;
+        let raw_sock = MockRawSocketWriter::new();
+        let local_addrs = HashSet::from([IpAddr::from_str("192.168.1.238").unwrap()]);
+        let (evict_tx, mut evict_rx) = mpsc::channel(10);
+        let mut connection_tracker = ConnectionTracker::new(
+            Some(evict_tx),
+            max_connections_per_tracker,
+            local_addrs.clone(),
+            raw_sock,
+        );
+
+        // Read the first 3 packets from the trace. I.e., just the handshake but no data.
+        // ==> No probes are sent.
+        let mut capture =
+            pcap::Capture::from_file(test_dir("tests/normal-conn-syn-and-fin.pcap")).unwrap();
+        let mut num_pks = 0;
+        while let Ok(pkt) = capture.next_packet() {
+            if num_pks >= 3 {
+                // just the 3-way handshake
+                break;
+            }
+            connection_tracker.add(OwnedParsedPacket::try_from(pkt).unwrap());
+            num_pks += 1;
+        }
+
+        assert_eq!(connection_tracker.connections.len(), 1);
+
+        // pause() halt updating of the tokio Instant timer.
+        tokio::time::pause();
+        // this sleep won't actually sleep but just advande the Instant timer
+        tokio::time::sleep(tokio::time::Duration::from_millis(TIME_WAIT_MS + 10)).await;
+
+        // Create a packet from a different connection.
+        let mut pkt_unrelated_conn_raw: Vec<u8> = Vec::new();
+        etherparse::PacketBuilder::ethernet2([1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12])
+            .ipv4([192, 168, 1, 238], [192, 168, 1, 2], 20)
+            .tcp(12345, 80, 42, 1024)
+            .write(&mut pkt_unrelated_conn_raw, &[])
+            .unwrap();
+        let pkt_unrelated_conn =
+            OwnedParsedPacket::try_from_fake_time(pkt_unrelated_conn_raw).unwrap();
+        let unrelated_conn_key = pkt_unrelated_conn
+            .clone()
+            .to_connection_key(&local_addrs)
+            .unwrap()
+            .0;
+
+        // and put the packet into connection tracker. The previous connection should
+        // be evicted, but since the previous connection doesn't have any probes, it
+        // should not have been sent to the storage handler
+        connection_tracker.add(pkt_unrelated_conn);
+        assert_eq!(connection_tracker.connections.len(), 1);
+        match evict_rx.try_recv() {
+            Err(mpsc::error::TryRecvError::Empty) => (),
+            x => panic!("Expected to get an empty from evict_rx, got {:?}", x),
+        }
 
         // make sure we have the "unrelated" connection in the tracker
         assert!(connection_tracker
