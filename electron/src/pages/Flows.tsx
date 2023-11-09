8cea4bc50247631ad6a6fec52b0fb9f7efbecff1
Gregor Maier <gregor@majordomus.org>
2023-11-08 20:43:45 -0500

    UI: Fix memory leak, add basic filtering and pause for flows
    Whatever gets logged to the JS console remains in memory and won't be
    GC'ed (presumably until the console reaches it line limit and evicts old
    line). We were logging large (MBs) objects we received from the server,
    this ran the JS instance out of memory. So lets not log the received
    object to the console.
    
    Also reducing log levels to debug in many cases.
    
    Finally, adding some functionality for the Flow tab: Add a very basic
    filter (better filtering to come soon) and ability to pause refreshes.
    
    Test Plan:
    See screenshot

--- electron/src/pages/Flows.tsx
+++ electron/src/pages/Flows.tsx
@@ -1,4 +1,10 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { ConnectionMeasurements } from "../netdebug_types";
 import useWebSocket from "react-use-websocket";
 import { WS_URL } from "../App";
@@ -14,14 +20,29 @@ import Table from "@mui/material/Table";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
+import { FormControlLabel, Switch } from "@mui/material";
 
-// build a connection key
-function getConnKey(conn: ConnectionMeasurements) {
+// build a unique connection key, using the same fields the
+// rust logic uses.
+function getUniqueConnKey(conn: ConnectionMeasurements) {
+  return (
+    `${conn.ip_proto}-${conn.local_ip}:${conn.local_l4_port}` +
+    `-[${conn.remote_ip}]:${conn.remote_l4_port}`
+  );
+}
+
+// build a connection key for displaying in the table
+// This key might not be unique. E.g., two different local IPs could
+// have the same local port and remote pair.
+// And/or a remote hostname might have to IPs.
+// While this is very unlikely, react gets very angry of `key` attributes
+// aren't unique.
+function getConnKeyForDisplay(conn: ConnectionMeasurements) {
   const remote =
     conn.remote_hostname !== null
       ? conn.remote_hostname
       : `[${conn.remote_ip}]`;
-  return `${conn.ip_proto} ::${conn.local_l4_port} --> ${remote}:${conn.remote_l4_port}`;
+  return `${conn.ip_proto} :${conn.local_l4_port} --> ${remote}:${conn.remote_l4_port}`;
 }
 
 function connSortFn(a: ConnectionMeasurements, b: ConnectionMeasurements) {
@@ -29,17 +50,39 @@ function connSortFn(a: ConnectionMeasurements, b: ConnectionMeasurements) {
   return dt;
 }
 
+type StateUpdateFn = Dispatch<SetStateAction<boolean>>;
+interface SwitchHelperProps {
+  text: string;
+  state: boolean;
+  updateFn: StateUpdateFn;
+}
+const SwitchHelper: React.FC<SwitchHelperProps> = (props) => {
+  return (
+    <FormControlLabel
+      control={<Switch />}
+      label={props.text}
+      checked={props.state}
+      onChange={() => {
+        props.updateFn(!props.state);
+      }}
+    />
+  );
+};
+
 const Flows: React.FC = () => {
   const [flowEntries, setFlowEntries] = useState(
     new Array<ConnectionMeasurements>(),
   );
+  const [autoRefresh, setAutoRefresh] = React.useState(true);
+  const [showUdp, setShowUdp] = React.useState(true);
+  const [showTcp, setShowTcp] = React.useState(true);
   const min_time_between_requests_ms = 1000;
   const max_time_between_requests_ms = 2000;
   const timeout_id = useRef(null);
   const last_send = useRef(null);
 
   const sendRequest = () => {
-    console.log("Sending DNS request");
+    console.log("Sending DumpFlows request");
     sendMessage(
       JSON.stringify({
         DumpFlows: [],
@@ -49,88 +92,117 @@ const Flows: React.FC = () => {
   };
   const { sendMessage } = useWebSocket(WS_URL, {
     onOpen: () => {
-      console.log("WebSocket connection established.");
+      console.debug("WebSocket connection established.");
     },
 
     onMessage: (msg) => {
       const data = JSON.parse(msg.data);
-      console.log("Got message from websocket: ", typeof data, data);
+      console.debug("Got message from websocket: ", typeof data);
       if ("DumpFlowsReply" in data) {
-        setFlowEntries(data.DumpFlowsReply);
-        periodic_with_sla(
-          "DumpFlowsReply",
-          timeout_id,
-          last_send,
-          min_time_between_requests_ms,
-          max_time_between_requests_ms,
-          sendRequest,
-        );
+        if (autoRefresh) {
+          setFlowEntries(data.DumpFlowsReply);
+          periodic_with_sla(
+            "DumpFlowsReply",
+            timeout_id,
+            last_send,
+            min_time_between_requests_ms,
+            max_time_between_requests_ms,
+            sendRequest,
+          );
+        }
       }
     },
 
+    onError: () => {
+      // If this happens, something is seriously wrong since the desktop
+      // process must not be running
+      alert("Error connecting to websocket");
+    },
+
     onClose: () => {
-      console.log("Closing websocket");
+      console.debug("Closing websocket");
     },
   });
 
   // send a DumFlows message one time on first load
+  // or if autoRefresh changes to "on"
   useEffect(() => {
-    sendRequest();
+    if (autoRefresh) {
+      sendRequest();
+    }
     return () => {
       // on unmount, clear the timeout, if it's set
       timeout_id && clearTimeout(timeout_id.current);
     };
-  }, []);
+  }, [autoRefresh]);
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow style={headerStyle}>
-            <TableCell sx={headerStyleWithWidth(0.3)} align="left">
-              Application(s)
-            </TableCell>
-            <TableCell sx={headerStyleWithWidth(0.1)} align="right">
-              Send Bandwidth
-            </TableCell>
-            <TableCell sx={headerStyleWithWidth(0.1)} align="right">
-              Recv Bandwidth
-            </TableCell>
-            <TableCell sx={headerStyleWithWidth(0.5)} align="left">
-              Flow Key
-            </TableCell>
-          </TableRow>
-          {flowEntries
-            .sort(connSortFn)
-            .filter((conn) => conn.ip_proto == "TCP")
-            .map((conn) => {
-              const key = getConnKey(conn);
-              let app: string;
-              if (conn.associated_apps === null) {
-                app = "(unknown)";
-              } else {
-                app = "";
-                Object.entries(conn.associated_apps).forEach(([x, y]) => {
-                  app += y === null ? `(${x})` : y;
-                });
-              }
-              const renderedKey = conn.four_way_close_done ? <s>{key}</s> : key;
-              return (
-                <TableRow key={key}>
-                  <TableCell>{app}</TableCell>
-                  <TableCell align="right">
-                    {rateEstimatorPrettyRate(conn.tx_byte_rate, "Bytes/s")}
-                  </TableCell>
-                  <TableCell align="right">
-                    {rateEstimatorPrettyRate(conn.rx_byte_rate, "Bytes/s")}
-                  </TableCell>
-                  <TableCell>{renderedKey}</TableCell>
-                </TableRow>
-              );
-            })}
-        </TableHead>
-      </Table>
-    </TableContainer>
+    <>
+      <SwitchHelper
+        text={"Auto Refresh"}
+        state={autoRefresh}
+        updateFn={setAutoRefresh}
+      />
+      <SwitchHelper text={"Show UDP"} state={showUdp} updateFn={setShowUdp} />
+      <SwitchHelper text={"Show TCP"} state={showTcp} updateFn={setShowTcp} />
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="Table of Connections">
+          <TableHead>
+            <TableRow style={headerStyle}>
+              <TableCell sx={headerStyleWithWidth(0.3)} align="left">
+                Application(s)
+              </TableCell>
+              <TableCell sx={headerStyleWithWidth(0.1)} align="right">
+                Send Bandwidth
+              </TableCell>
+              <TableCell sx={headerStyleWithWidth(0.1)} align="right">
+                Recv Bandwidth
+              </TableCell>
+              <TableCell sx={headerStyleWithWidth(0.5)} align="left">
+                Flow Key
+              </TableCell>
+            </TableRow>
+            {flowEntries
+              .sort(connSortFn)
+              .filter((conn) => {
+                return (
+                  (showTcp && conn.ip_proto == "TCP") ||
+                  (showUdp && conn.ip_proto == "UDP")
+                );
+              })
+              .map((conn) => {
+                let app: string;
+                if (conn.associated_apps === null) {
+                  app = "(unknown)";
+                } else {
+                  app = "";
+                  Object.entries(conn.associated_apps).forEach(([x, y]) => {
+                    app += y === null ? `(${x})` : y;
+                  });
+                }
+                const displayedKey = getConnKeyForDisplay(conn);
+                const renderedKey = conn.four_way_close_done ? (
+                  <s>{displayedKey}</s>
+                ) : (
+                  displayedKey
+                );
+                return (
+                  <TableRow key={getUniqueConnKey(conn)}>
+                    <TableCell>{app}</TableCell>
+                    <TableCell align="right">
+                      {rateEstimatorPrettyRate(conn.tx_byte_rate, "Bytes/s")}
+                    </TableCell>
+                    <TableCell align="right">
+                      {rateEstimatorPrettyRate(conn.rx_byte_rate, "Bytes/s")}
+                    </TableCell>
+                    <TableCell>{renderedKey}</TableCell>
+                  </TableRow>
+                );
+              })}
+          </TableHead>
+        </Table>
+      </TableContainer>
+    </>
   );
 };
 
