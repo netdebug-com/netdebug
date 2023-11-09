bbc104d0ec7d5d81216aaec0959153eddd3d86b8
Rob Sherwood <135524607+capveg-netdebug@users.noreply.github.com>
2023-11-08 20:10:31 -0800

    Moved webserver to sync pcap calls, so it works on windows (#235)
    TSIA
    
    See #234 for something else that I saw was broken, but let's figure
    that out later.

--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -1,21 +1,12 @@
-#[cfg(not(windows))]
-use libconntrack::pcap::start_pcap_stream;
-#[cfg(not(windows))]
 use libwebserver::context::{Args, WebServerContext};
 use std::error::Error;
-#[cfg(not(windows))]
 use std::sync::Arc;
 
-#[cfg(not(windows))]
 use clap::Parser;
-#[cfg(not(windows))]
 use libwebserver::http_routes::make_webserver_http_routes;
-#[cfg(not(windows))]
 use log::info;
-#[cfg(not(windows))]
 use tokio::sync::RwLock;
 
-#[cfg(not(windows))]
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     common::init::netdebug_init();
@@ -24,24 +15,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     // init webserver state
     let context = Arc::new(RwLock::new(WebServerContext::new(&args)?));
-    let context_clone = context.clone();
     if !args.web_server_only {
-        tokio::spawn(async move {
-            let (device, local_tcp_port, tx) = {
-                let ctx = context_clone.read().await;
-                (
-                    ctx.pcap_device.clone(),
-                    ctx.local_tcp_listen_port,
-                    ctx.connection_tracker.clone(),
-                )
-            };
-            // unwrap() should be fine here as we should panic if this fails
-            if let Err(e) = start_pcap_stream(device, local_tcp_port, tx).await {
-                log::error!("start_pcap_stream() returned {} -- exiting", e);
-                // this is fatal, just exit
-                std::process::exit(1);
-            }
-        });
+        let (device_name, tcp_listen_port, connection_tx) = {
+            let ctx = context.read().await;
+            (
+                ctx.pcap_device.name.clone(),
+                ctx.local_tcp_listen_port,
+                ctx.connection_tracker.clone(),
+            )
+        };
+        let _pcap_thread = libconntrack::pcap::run_blocking_pcap_loop_in_thread(
+            device_name,
+            Some(format!("tcp port {}", tcp_listen_port)),
+            connection_tx,
+            None,
+        );
     }
 
     info!(
@@ -65,10 +53,4 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .run(listen_addr)
         .await;
     Ok(())
-}
-
-#[cfg(windows)]
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    panic!("Webserver not supported on windows - even if some of it compiles");
 }
