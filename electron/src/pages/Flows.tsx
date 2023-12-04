2014fe576636121f0641e468e974ecf44c72c0d5
Gregor Maier <gregor.maier@netdebug.com>
2023-12-04 11:32:18 -0500

    UI: use DataGrid for flows tab (by Flows)
     ### Summary:
     as titled.
    
     ### Test Plan:
     screenshot

--- electron/src/pages/Flows.tsx
+++ electron/src/pages/Flows.tsx
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import { ConnectionMeasurements } from "../netdebug_types";
 import {
-  connKeyToStr,
-  headerStyle,
-  headerStyleWithWidth,
+  dataGridDefaultSxProp,
   prettyPrintSiUnits,
+  sortCmpWithNull,
 } from "../utils";
-import TableContainer from "@mui/material/TableContainer";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import TableCell from "@mui/material/TableCell";
 import { SwitchHelper } from "../components/SwitchHelper";
 import { useWebSocketGuiToServer } from "../useWebSocketGuiToServer";
+import { Box } from "@mui/material";
+import {
+  DataGrid,
+  GridColDef,
+  GridToolbar,
+  GridValueFormatterParams,
+  GridValueGetterParams,
+} from "@mui/x-data-grid";
 
 // build a connection key for displaying in the table
 // This key might not be unique. E.g., two different local IPs could
@@ -29,23 +30,78 @@ function getConnKeyForDisplay(conn: ConnectionMeasurements) {
   return `${conn.key.ip_proto} ${conn.key.local_l4_port} --> ${remote}:${conn.key.remote_l4_port}`;
 }
 
-function connSortFn(a: ConnectionMeasurements, b: ConnectionMeasurements) {
-  const max_rate_fn = (conn: ConnectionMeasurements) => {
-    return Math.max(
-      conn.tx_stats.last_min_byte_rate,
-      conn.rx_stats.last_min_byte_rate,
-    );
-  };
-  return max_rate_fn(b) - max_rate_fn(a);
+function formatAssociatedApps(
+  params: GridValueFormatterParams<ConnectionMeasurements["associated_apps"]>,
+) {
+  let app: string;
+  if (params.value === null) {
+    app = "(unknown)";
+  } else {
+    app = "";
+    Object.entries(params.value).forEach(([x, y]) => {
+      app += y === null ? `(${x})` : y;
+    });
+  }
+  return app;
 }
+
+const defaultRateGridColDef: {
+  valueFormatter: GridColDef["valueFormatter"];
+  align: GridColDef["align"];
+  flex: number;
+  headerAlign: GridColDef["align"];
+  sortComparator: GridColDef["sortComparator"];
+} = {
+  valueFormatter: (params: GridValueFormatterParams<number>) =>
+    prettyPrintSiUnits(params.value, "B/s"),
+  align: "right",
+  flex: 10,
+  headerAlign: "right",
+  sortComparator: sortCmpWithNull,
+};
+
+const columns: GridColDef[] = [
+  {
+    // Note, this field doesn't actually exist in ConnectionMeasurement. We use `valueGetter`
+    // to derive the actual value of this field, but DataGrid still requires a unique field name
+    // (e.g., for sort state, etc.)
+    field: "Flow Key",
+    hideable: false,
+    flex: 50,
+    valueGetter: (params: GridValueGetterParams<ConnectionMeasurements>) =>
+      getConnKeyForDisplay(params.row),
+  },
+  {
+    // Note, this field doesn't actually exist in ConnectionMeasurement. We use `valueGetter`
+    // to derive the actual value of this field, but DataGrid still requires a unique field name
+    // (e.g., for sort state, etc.)
+    field: "send_bw",
+    headerName: "Send B/W",
+    valueGetter: (params: GridValueGetterParams<ConnectionMeasurements>) =>
+      params.row.tx_stats?.last_min_byte_rate,
+    ...defaultRateGridColDef,
+  },
+  {
+    // Note, this field doesn't actually exist in ConnectionMeasurements
+    field: "recv_bw",
+    headerName: "Recv B/W",
+    valueGetter: (params: GridValueGetterParams<ConnectionMeasurements>) =>
+      params.row.rx_stats?.last_min_byte_rate,
+    ...defaultRateGridColDef,
+  },
+  {
+    field: "associated_apps",
+    headerName: "Associated Apps",
+    flex: 30,
+    valueFormatter: formatAssociatedApps,
+  },
+];
 
 const Flows: React.FC = () => {
   const [flowEntries, setFlowEntries] = useState(
     new Array<ConnectionMeasurements>(),
   );
   const [autoRefresh, setAutoRefresh] = useState(true);
-  const [showUdp, setShowUdp] = useState(true);
-  const [showTcp, setShowTcp] = useState(true);
 
   useWebSocketGuiToServer({
     autoRefresh: autoRefresh,
@@ -53,7 +109,17 @@ const Flows: React.FC = () => {
     respMsgType: "DumpFlowsReply",
     min_time_between_requests_ms: 1000,
     max_time_between_requests_ms: 2000,
-    responseCb: setFlowEntries,
+    responseCb: (flows: ConnectionMeasurements[]) => {
+      // DataGrid can also be "unsorted" when not sort is applied to any columns. That looks ugly,
+      // so presort the rows into a reasonable default order
+      flows.sort((a, b) =>
+        sortCmpWithNull(
+          b.rx_stats?.last_min_byte_rate,
+          a.rx_stats?.last_min_byte_rate,
+        ),
+      );
+      setFlowEntries(flows);
+    },
   });
 
   return (
@@ -63,75 +129,29 @@ const Flows: React.FC = () => {
         state={autoRefresh}
         updateFn={setAutoRefresh}
       />
-      <SwitchHelper text={"Show UDP"} state={showUdp} updateFn={setShowUdp} />
-      <SwitchHelper text={"Show TCP"} state={showTcp} updateFn={setShowTcp} />
-      <TableContainer component={Paper}>
-        <Table
-          sx={{ minWidth: 650 }}
-          size="small"
+      <Box width="100%">
+        <DataGrid
           aria-label="Table of Connections"
-        >
-          <TableHead>
-            <TableRow style={headerStyle}>
-              <TableCell sx={headerStyleWithWidth(0.3)} align="left">
-                Application(s)
-              </TableCell>
-              <TableCell sx={headerStyleWithWidth(0.1)} align="right">
-                Send Bandwidth
-              </TableCell>
-              <TableCell sx={headerStyleWithWidth(0.1)} align="right">
-                Recv Bandwidth
-              </TableCell>
-              <TableCell sx={headerStyleWithWidth(0.5)} align="left">
-                Flow Key
-              </TableCell>
-            </TableRow>
-            {flowEntries
-              .sort(connSortFn)
-              .filter((conn) => {
-                return (
-                  (showTcp && conn.key.ip_proto == "TCP") ||
-                  (showUdp && conn.key.ip_proto == "UDP")
-                );
-              })
-              .map((conn) => {
-                let app: string;
-                if (conn.associated_apps === null) {
-                  app = "(unknown)";
-                } else {
-                  app = "";
-                  Object.entries(conn.associated_apps).forEach(([x, y]) => {
-                    app += y === null ? `(${x})` : y;
-                  });
-                }
-                const displayedKey = getConnKeyForDisplay(conn);
-                const renderedKey = conn.four_way_close_done ? (
-                  <s>{displayedKey}</s>
-                ) : (
-                  displayedKey
-                );
-                return (
-                  <TableRow key={connKeyToStr(conn.key)}>
-                    <TableCell>{app}</TableCell>
-                    <TableCell align="right">
-                      {prettyPrintSiUnits(
-                        conn.tx_stats?.last_min_byte_rate,
-                        "Bytes/s",
-                      )}
-                    </TableCell>
-                    <TableCell align="right">
-                      {prettyPrintSiUnits(
-                        conn.rx_stats?.last_min_byte_rate,
-                        "Bytes/s",
-                      )}
-                    </TableCell>
-                    <TableCell>{renderedKey}</TableCell>
-                  </TableRow>
-                );
-              })}
-          </TableHead>
-        </Table>
-      </TableContainer>
+          density="compact"
+          columns={columns}
+          rows={flowEntries}
+          sx={{
+            width: "100%",
+            ...dataGridDefaultSxProp,
+          }}
+          initialState={{
+            sorting: {
+              sortModel: [{ field: "recv_bw", sort: "desc" }],
+            },
+          }}
+          slots={{
+            toolbar: GridToolbar,
+          }}
+          slotProps={{
+            toolbar: { printOptions: { disableToolbarButton: true } },
+          }}
+        />
+      </Box>
     </>
   );
 };
