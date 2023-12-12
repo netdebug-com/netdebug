1b7a867eb21903f306aabea0b45dfe3ef748c86b
Rob Sherwood <135524607+capveg-netdebug@users.noreply.github.com>
2023-12-12 15:38:29 -0800

    Update server maintenance scripts (#390)
    ### Summary:
    
    Moved running of our webserver process to systemd (yay!) with
    syslogging (yay!) and implicit log rotation.
    
    Added 'update_all_webservers.sh' to auto update all of the servers
    listed in netdebug.com/vantages.json
    
    Added a bunch of checks/more stuff in 'prep_server.sh'
    
    TODO: drop privs as everything is running as root for now...
    
     ### Test Plan:
    
    lots of manual testing... need to prove it out more
    
    ---
    
    <details open="true"><summary>Generated summary (powered by <a href="https://app.graphite.dev">Graphite</a>)</summary>
    
    > ## TL;DR
    > This pull request introduces several changes to the project. It adds a new systemd service file for the Netdebug Webserver, updates the `prep_server.sh` script, adds a new Python script `update_all_webservers.py`, and modifies the `update_webserver.sh` script.
    > 
    > ## What changed
    > - Added a new systemd service file `netdebug-webserver.service` for the Netdebug Webserver.
    > - Updated the `prep_server.sh` script to include support for Ubuntu LTS 22.04 - "jammy" and install additional dependencies.
    > - Added a new Python script `update_all_webservers.py` to update multiple web servers.
    > - Modified the `update_webserver.sh` script to fix syntax errors and download the latest release.
    > 
    > ## How to test
    > 1. Replace the existing `netdebug-webserver.service` file with the new one.
    > 2. Run the `prep_server.sh` script on an Ubuntu LTS 22.04 - "jammy" machine and ensure it installs the required dependencies.
    > 3. Execute the `update_all_webservers.py` script and verify that it updates all the specified web servers.
    > 4. Run the `update_webserver.sh` script and check if it successfully downloads the latest release.
    > 
    > ## Why make this change
    > - The new systemd service file provides a standardized way to manage the Netdebug Webserver as a service.
    > - The `prep_server.sh` script needed updates to support the specific Ubuntu LTS 22.04 - "jammy" version and install additional dependencies.
    > - The `update_all_webservers.py` script allows for easier and automated updating of multiple web servers.
    > - The modifications to the `update_webserver.sh` script fix syntax errors and improve the process of downloading the latest release.
    </details>

--- scripts/prep_server.sh
+++ scripts/prep_server.sh
@@ -4,10 +4,20 @@
 # TODO: proper chef/puppet automation
 set -x
 
+# this is intended for Ubuntu LTS 22.04 - "jammy"
+
+if [ `lsb_release -c -s` != "jammy" ] ; then
+    lsb_release -a
+    echo "Are you sure this is Ubuntu Jammy?" >&2
+    exit 1
+else
+    echo "Correctly running Ubuntu 22.04 Jammy"
+fi
+
 sudo apt update
 
-## install any deps here
-sudo apt install libpcap
+## install any deps here; for jammy, libpcap is v0.8 in the package
+sudo apt install libpcap0.8 certbot
 
 ## install the 'gh' command
 type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
@@ -17,8 +27,40 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 && sudo apt update \
 && sudo apt install gh -y
 
+# create signed ssl cert with certbot!
+echo "Enter 'webmaster@netdebug.com' for email and the full server name"
+echo " (e.g., "bay1.topology.netdebug.com") for the domain name"
+echo " Make sure you've allocated the DNS name *before* this as it's used for auth"
+sudo certbot certonly --standalone
+# setup compat symlink so that every machine finds the ssl in the same place
+# (without messing with where certbot expects them to be)
+# NOTE: certbot will also create a cronjob to auto-renew certs - magic!
+ln -s /etc/letsencrypt/live/*topology.netdebug.com /etc/letsencrypt/live/topology.netdebug.com
+
 ## install a user 'deploy' for us to install into
-sudo adduser deploy
+echo "Hit <enter> to enter blanks for real name, office, etc..."
+sudo adduser deploy --disabled-password
 sudo addgroup deploy sudo
 
+if [ -f update_webserver.sh ]; then
+    mv update_webserver.sh ~deploy
+    chown deploy:deploy ~deploy/update_webserver.sh
+else
+    echo "No update-webserver.sh found!? " >&2
+    exit 1
+
+fi
+
+SYSTEMD=netdebug-webserver.service
+if [ -f $SYSTEMD ] ; then
+    cp $SYSTEMD /etc/systemd/system
+    systemctl start netdebug-webserver
+    systemctl enable netdebug-webserver
+else 
+    echo "No $SYSTEMD found!? " >&2
+    exit 1
+fi
+
 # TODO: make sure the root user is sane - currently on Equinix metal you can log in as root by default
+echo "Now manually copy .github_access_token into ~deploy"
+echo "Now manually set group 'sudo' with NOPASSWD: "
--- scripts/run_webserver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-## TODO
-# Think about logrotation, connection db management, etc.
-
-exec sudo ./target/release/webserver --production --listen-port 443 |& tee webserver.logs
