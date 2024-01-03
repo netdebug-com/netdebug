2a1586503c5b2f51c95fbaf501125158431156f1
Rob Sherwood <135524607+capveg-netdebug@users.noreply.github.com>
2024-01-03 10:00:09 -0800

    FIXED - An Agent to talk to TimescaleDB (#395)
    ### Summary:
    
    Create a 'RemoteDB_Client' agent to wrap all of the postgres
    stuff and talk to Timescaledb.com where we have newly magiced
    cloud storage.  
    
    Push our counter data there.
    
    This is in theory just postgres so we use the default
    tokio-postgres SQL client for rust.  Seems mostly reasonable.
    
    
     ### Test Plan:
    
    System-level DB tests!  I've found a package 'pg_embed' which magically
    downloads, starts up and cleans up after itself for postgresql testing.
    It seems to work fairly well (now that I've got it configured correctly).
    If you're using the debugger and kill things off prematurely you do have
    to manually kill off the postgres daemon and the error message
     isn't that obvious when you start a second one on the same port,
    but seems fairly stable.  Yay real testing!

---
+++ webserver/docs/DB_ADMIN.md
@@ -0,0 +1,41 @@
+# Notes for various timescaledb/postgres admin tasks
+
+1. Install 'psql'
+    ```
+    sudo apt install postgresql-client-common
+    ```
+2. Get an admin password from the website or Rob; store in ```$HOME/.timescaledb_auth```
+    (no, there does not yet appear to be an OAUTH system.. I think)
+3. The URL for our private db is 
+    ```
+    postgres://tsdbadmin@ttfd71uhz4.m8ahrqo1nb.tsdb.cloud.timescale.com:33628/tsdb?sslmode=require
+    ```
+4. Launch the psql command with url, enter password to get to the admin prompt
+    ```
+    psql postgres://tsdbadmin@ttfd71uhz4.m8ahrqo1nb.tsdb.cloud.timescale.com:33628/tsdb?sslmode=require
+    ```
+    'psql' has a bunch of useful commands you can find with '\?', e.g., list tables `\dt` or list schema `\d desktop_counters`
+
+
+# One time setup/database initialization
+
+1. Create the 'desktop_counters' table with the schema from `RemoteDBClient::create_table_schema()`, e.g., :
+    ```
+    CREATE TABLE desktop_counters ( counter VARCHAR(256), value BIGINT, os VARCHAR(128), version INT, source VARCHAR(256), time TIMESTAMPTZ);
+    ```
+2. Mark it as a 'hypertable' which tells the backend to treat it with timeseries optimizations:
+    ```
+    SELECT create_hypertable('desktop_counters', by_range('time'));
+    ```
+3. Set a data retention policy for how long we hold on to data.  This will auto-delete data older than 30 days.
+    Read more about it here:
+    https://docs.timescale.com/use-timescale/latest/data-retention/
+    ```
+    SELECT add_retention_policy('desktop_counters', INTERVAL '30 days');
+    ```
+4. TODO: setup 'continuous aggregates' to continuously downsample our data
+    https://docs.timescale.com/use-timescale/latest/data-retention/data-retention-with-continuous-aggregates/
+    NOT DONE YET!
+
+5. TODO: Consider using Timescale's "tiered storage" because we don't need high performance on this data;
+    details are here: https://docs.timescale.com/use-timescale/latest/data-tiering/
---
+++ webserver/tests/test_remotedb_client.rs
@@ -0,0 +1,163 @@
+use chrono::Utc;
+use indexmap::IndexMap;
+use libwebserver::remotedb_client::RemoteDBClient;
+use pg_embed::pg_fetch::{PgFetchSettings, PG_V13};
+use pg_embed::pg_types::PgResult;
+use pg_embed::postgres::{PgEmbed, PgSettings};
+use rand::Rng;
+use std::{path::PathBuf, time::Duration};
+use tokio_postgres::types::ToSql;
+use tokio_postgres::Client;
+
+use pg_embed::pg_enums::PgAuthMethod;
+
+/**
+ * Test against FAKE credentials!
+ *
+ */
+#[tokio::test]
+async fn test_remotedb_client() {
+    let (client, db) = mk_test_db("testdb-test_remotedb_client").await.unwrap();
+    let remotedb_client = RemoteDBClient::mk_mock(&db.db_uri);
+    remotedb_client.create_table_schema(&client).await.unwrap();
+    let now = Utc::now();
+    let alice_counters =
+        IndexMap::from_iter([("count1".to_string(), 2), ("count2".to_string(), 43)]);
+    remotedb_client
+        .handle_store_counters(&client, alice_counters, "alice".to_string(), now)
+        .await
+        .unwrap();
+    let rows = remotedb_client
+        .count_remote_log_rows(&client)
+        .await
+        .unwrap();
+    assert_eq!(rows, 2);
+    let bob_counters = IndexMap::from_iter([("count1".to_string(), 2), ("count2".to_string(), 43)]);
+    remotedb_client
+        .handle_store_counters(&client, bob_counters, "bob".to_string(), now)
+        .await
+        .unwrap();
+    let rows = remotedb_client
+        .count_remote_log_rows(&client)
+        .await
+        .unwrap();
+    assert_eq!(rows, 4);
+}
+/**
+ * Does the embedded DB thing work?
+ * Simple write and read back test with none of our code for sanity
+ */
+#[tokio::test]
+async fn test_pm_embed() {
+    let (client, _db) = mk_test_db("testdb").await.unwrap();
+    client
+        .execute(
+            "CREATE TABLE test_pm_embed (num INT, name varchar(265))",
+            &[],
+        )
+        .await
+        .unwrap();
+    for (num, name) in [(1, "Alice"), (2, "Bob"), (3, "Cathy")] {
+        // OMG look at this type!!  Fought for a while and then had to C&P from stackoverflow :-(
+        let num = &num as &(dyn ToSql + Sync);
+        let affected_rows = client
+            .execute(
+                "INSERT INTO test_pm_embed (num, name) VALUES ($1, $2)",
+                &[num, &name],
+            )
+            .await
+            .unwrap();
+        assert_eq!(affected_rows, 1);
+    }
+    let matched_rows = client
+        .execute("SELECT * FROM test_pm_embed", &[])
+        .await
+        .unwrap();
+    assert_eq!(matched_rows, 3);
+}
+
+async fn mk_test_db(database_name: &str) -> PgResult<(Client, PgEmbed)> {
+    let db_dir = random_db_dir();
+    // https://github.com/faokunega/pg-embed/issues/31
+    // can't bind '0' for an ephemeral port :-( so just fake it and pray :-()
+    let port = rand::thread_rng().gen_range(1024..65000);
+    // Postgresql settings; all copied directly from documentation
+    let pg_settings = PgSettings {
+        // Where to store the postgresql database
+        database_dir: db_dir.clone(),
+        port,
+        user: "postgres".to_string(),
+        password: "password".to_string(),
+        // authentication method
+        auth_method: PgAuthMethod::Plain,
+        // If persistent is false clean up files and directories on drop, otherwise keep them
+        persistent: true, // keep for now?
+        // duration to wait before terminating process execution
+        // pg_ctl start/stop and initdb timeout
+        // if set to None the process will not be terminated
+        timeout: Some(Duration::from_secs(15)),
+        // If migration sql scripts need to be run, the directory containing those scripts can be
+        // specified here with `Some(PathBuf(path_to_dir)), otherwise `None` to run no migrations.
+        // To enable migrations view the **Usage** section for details
+        migration_dir: None,
+    };
+
+    // Postgresql binaries download settings
+    let fetch_settings = PgFetchSettings {
+        version: PG_V13,
+        ..Default::default()
+    };
+
+    // Create a new instance
+    let mut pg = PgEmbed::new(pg_settings, fetch_settings).await?;
+
+    // Download, unpack, create password file and database cluster
+    pg.setup().await?;
+
+    // start postgresql database
+    pg.start_db().await?;
+
+    // create a new database
+    // to enable migrations view the [Usage] section for details
+    pg.create_database(database_name).await?;
+
+    // drop a database
+    // to enable migrations view [Usage] for details
+    // pg.drop_database("database_name").await;
+
+    // check database existence
+    // to enable migrations view [Usage] for details
+    // pg.database_exists("database_name").await;
+
+    // run migration sql scripts
+    // to enable migrations view [Usage] for details
+    // pg.migrate("database_name").await;
+
+    // stop postgresql database
+    // pg.stop_db().await;
+    // `postgres://{username}:{password}@localhost:{port}`
+    // let pg_uri: &str = &pg.db_uri;
+
+    // get a postgresql database uri
+    // `postgres://{username}:{password}@localhost:{port}/{specified_database_name}`
+    // let pg_db_uri: String = pg.full_db_uri("database_name");
+    let (client, connection) = tokio_postgres::connect(&pg.db_uri, tokio_postgres::NoTls)
+        .await
+        .unwrap();
+    tokio::spawn(async move {
+        connection.await.unwrap();
+    });
+    Ok((client, pg))
+}
+
+fn random_db_dir() -> PathBuf {
+    let mut rng = rand::thread_rng();
+    let random: u32 = rng.gen();
+    let mut db_path = if let Ok(_metadata) = std::fs::metadata("/tmp") {
+        PathBuf::from("/tmp")
+    } else {
+        PathBuf::from(".")
+    };
+    db_path.push(format!("netdebug-db-test-{}", random));
+    db_path
+}
