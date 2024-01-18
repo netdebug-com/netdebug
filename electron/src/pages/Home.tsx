12612583952cb407b9680aa084bff26bfb9196d1
Gregor Maier <gregor.maier@netdebug.com>
2024-01-18 16:22:16 -0500

    UI Home page: use "manual" data loading w/ error handling
     ### Summary:
     This allows us to load multiple independent pieces of data for the
     homepage (and/or add it to other pages that otherwise already use a
     data loader for the bulk of their content).
    
     ### Test Plan:
     screenshots

--- electron/src/pages/Home.tsx
+++ electron/src/pages/Home.tsx
@@ -1,24 +1,36 @@
-import React from "react";
 import { desktop_api_url } from "../utils";
-import { useLoaderData } from "react-router";
+import { useInterval } from "react-use";
+import { useEffect, useState } from "react";
+import { DataLoadingState, loadData as loadData } from "../common/data_loading";
 
-// FIXME: need to use a different pattern to load the data.
-// (a) this request needs to go to the topology server so it could take a while
-//     (react-router / useDataLoader has "Loading..." indicators. Need to investigate
-// (b) we want to call multiple different APIs to populate the homepage ==> find best way
-//     to do that.
-export const myIpLoader = async () => {
-  const res = await fetch(desktop_api_url("get_my_ip"));
-  return res.json();
-};
+function renderStringData(state: DataLoadingState<string>) {
+  return (
+    <>
+      {state.isPending && "Loading ..."}
+      {state.error && (
+        <div style={{ color: "red", fontWeight: "bold" }}>
+          ERROR: {state.error}
+        </div>
+      )}
+      {state.data && state.data}
+    </>
+  );
+}
 
 const Home: React.FC = () => {
-  const myIp = useLoaderData() as string;
+  const [myIp, setMyIp] = useState(new DataLoadingState<string>());
+  useEffect(() => {
+    loadData(desktop_api_url("get_my_ip"), setMyIp);
+  }, []);
+  // Just for demo purposes: how to periodically reload the data.
+  useInterval(() => {
+    loadData(desktop_api_url("get_my_ip"), setMyIp);
+  }, 5000);
 
   return (
     <div>
       <h1>Home Page</h1>
-      My external IP Address is <em>{myIp}</em>.
+      My external IP Address is <em>{renderStringData(myIp)}</em>.
     </div>
   );
 };
