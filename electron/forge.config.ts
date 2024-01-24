86a119e1970ce9db57e6c0a7c27115576d8067a9
Gregor Maier <143829239+gregor-netdebug@users.noreply.github.com>
2024-01-24 14:08:54 -0500

    Electron: Add APP icons (#475)
    ### Summary:
     as titled. One would think it's well documented how to create the Mac
     OS icon bundles (.icns files) but it's what. What a joy. At least the
     .ico is straight forward.
    
    The icon needs a polishing but better than the stock Electron one. 
    
     ### Test Plan:
     `npm run package`, then started the App => shows up with our icon in
     dock
     not tested on windows.
    
    ![Screenshot 2024-01-23 at 9.13.02 PM.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/NGRnlVz3aWBiLb2etNoW/3cd31936-f1dd-43b3-a17c-9b58e61be5d2.png)

--- electron/forge.config.ts
+++ electron/forge.config.ts
@@ -1,8 +1,8 @@
 import type { ForgeConfig } from "@electron-forge/shared-types";
 import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
-import { MakerDeb } from "@electron-forge/maker-deb";
-import { MakerRpm } from "@electron-forge/maker-rpm";
+//import { MakerDeb } from "@electron-forge/maker-deb";
+//import { MakerRpm } from "@electron-forge/maker-rpm";
 import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
 import { WebpackPlugin } from "@electron-forge/plugin-webpack";
 
@@ -13,13 +13,18 @@ const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
     extraResource: "extra-resources",
+    // see https://www.electronforge.io/guides/create-and-add-icons
+    // extensions is automatically added based on target platform:
+    // .icns for Mac; .ico for Windows. joy
+    icon: "src/images/icon",
   },
   rebuildConfig: {},
   makers: [
     new MakerSquirrel({}),
     new MakerZIP({}, ["darwin"]),
-    new MakerRpm({}),
-    new MakerDeb({}),
+    // disable linux for now
+    // new MakerRpm({}),
+    // new MakerDeb({}),
   ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
