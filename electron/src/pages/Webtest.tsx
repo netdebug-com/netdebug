2ba7d9b0fef753a22017f8faa3ceb2207b9990f1
Gregor Maier <143829239+gregor-netdebug@users.noreply.github.com>
2024-01-25 19:29:02 -0500

    UI: Webtest: make server location selectable (#488)
    ### Summary:
     we now have a selection drop-down to select the location of the server.
     Currently it's hard-coded rather than using vantage-points.json.
     Will add using the json file in a follow up PR.
    
     ### Test Plan:
     see screenshots

--- electron/src/pages/Webtest.tsx
+++ electron/src/pages/Webtest.tsx
@@ -1,13 +1,67 @@
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+import { useState } from "react";
+
+function getVantageUrl(vantagePoint: string) {
+  const urlSuffix = "topology.netdebug.com/webtest_8338550042";
+  switch (vantagePoint) {
+    case "bay1":
+    case "nyc1":
+      return `https://${vantagePoint}.${urlSuffix}`;
+    case "dal1":
+      // the cert for dal1.topology.netdebug.com is currently busted. It's CN is
+      // just `topology.netdebug.com`. So we need to return that as URL otherwise
+      // we get SSL errors.
+      return `https://${urlSuffix}`;
+    default:
+      return null;
+  }
+}
+
 export default function Webtest() {
+  const [vantage, setVantage] = useState("");
+
+  const url = getVantageUrl(vantage);
+  console.log("Using vantage point:", url ? url : "None");
   return (
-    // The webtest page uses a fixed height as far as I can tell. So lets
-    // hardcode this here, otherwise the iframe looks ugly with scroll bars and
-    // all...
-    // TODO: replace this iframe with a native UI and talk to the webserver
-    // over RPC (via the desktop probably)
-    <iframe
-      src="https://topology.netdebug.com/webtest_8338550042"
-      style={{ width: "100%", height: "950px" }}
-    />
+    <>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <FormControl sx={{ width: "300px" }}>
+          <InputLabel id="webtest-vantage-selection-label">
+            Server Location
+          </InputLabel>
+          <Select
+            id="webtest-vantage-selection"
+            labelId="webtest-vantage-selection-label"
+            value={vantage}
+            onChange={(ev: SelectChangeEvent) => setVantage(ev.target.value)}
+            label="Server Location"
+            fullWidth
+          >
+            <MenuItem value="bay1">SF Bay Area</MenuItem>
+            <MenuItem value="dal1">Texas</MenuItem>
+            <MenuItem value="nyc1">New York</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      {
+        // The webtest page uses a fixed height as far as I can tell. So lets
+        // hardcode this here, otherwise the iframe looks ugly with scroll bars and
+        // all...
+        // TODO: replace this iframe with a native UI and talk to the webserver
+        // over RPC (via the desktop probably)
+      }
+      {url && <iframe src={url} style={{ width: "100%", height: "950px" }} />}
+      {!url && (
+        <div style={{ padding: "30px" }}>
+          Select a server location to start the test
+        </div>
+      )}
+    </>
   );
 }
