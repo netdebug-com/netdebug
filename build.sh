58988c94ef39cdf515f4c0b0a27faaef1fedba12
Rob Sherwood <rob.sherwood@gmail.com>
2024-02-20 09:48:38 -0800

    Minor: update the ./build.sh script for 'netdebug-desktop' name

--- build.sh
+++ build.sh
@@ -24,5 +24,5 @@ wasm-pack build --target=web webserver/web-client ${build_opt}
 #     think this does weird things to currently running processes using 
 #     that binary. This way, the new binary will get a new inode and the existing
 #     processe won't see their binary modified underneath them. At least on MacOS
-rm -f electron/extra-resources/desktop
-cp $outpath/desktop electron/extra-resources
+rm -f electron/extra-resources/netdebug-desktop
+cp $outpath/netdebug-desktop electron/extra-resources
