41112c348efe2f268e047d4c3e3ce20e5d0df1c7
Gregor Maier <gregor.maier@netdebug.com>
2024-02-27 12:03:12 -0500

    Bump version to v.0.2.3 and update release notes
     ### Summary:
     if the release notes look good to you, I'll build an deploy a new
     Mac OS bundle
    
     ### Test Plan:
     screenshot

--- electron/src/pages/ReleaseNotes.tsx
+++ electron/src/pages/ReleaseNotes.tsx
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom";
-
 export const ReleaseNotes: React.FC = () => {
   const ul_style = {};
 
@@ -14,43 +12,25 @@ export const ReleaseNotes: React.FC = () => {
 
   return (
     <ul style={ul_style}>
-      <b>Release 0.2.2 - Feb 9th, 2024</b>
+      <b>Release 0.2.3 - Feb 27th, 2024</b>
       <li style={li_top_style}> Initial "Technical Preview Release!</li>
       <li style={li_top_style}>
-        Supports:
+        Many bugfixes and UI improvements:
         <ul style={ul_style}>
+          <li style={li_style}>Plot router RTT as histogram.</li>
+          <li style={li_style}>Polish local network tab.</li>
+          <li style={li_style}>Fix "No v4 Gateways" rendering bug.</li>
           <li style={li_style}>
-            Per-connection <Link to={"/rtt_latency"}> latency</Link> and
-            bi-directional <Link to={"/flows"}> loss information</Link>{" "}
-            (aggregated by <Link to={"/flows/by_dst_domain"}>DNS domain</Link>{" "}
-            and <Link to={"/flows/by_app"}>App</Link>)
+            Don't assume IPv4 link-local IP to Mac mapping.
           </li>
           <li style={li_style}>
-            Local network <Link to={"/devices"}>device</Link>,{" "}
-            <Link to={"/local_network"}> packet-loss, and latency </Link>
-            monitoring
-          </li>
-          <li style={li_style}>
-            Domain Name System resolver <Link to={"/dns"}>performance</Link>{" "}
-            analysis.
-          </li>
-          <li style={li_style}>
-            Ingress traffic testing via remote{" "}
-            <Link to={"/webtest"}>web test</Link>{" "}
-          </li>
-          <li style={li_style}>
-            Supports automated updates for MacOS and Windows (TODO: Linux
-            support)
-          </li>
-          <li style={li_style}>
-            Supports remote logging of measurements, counters, and logs
+            Make "By DNS domain" the default view for flows"
           </li>
         </ul>
       </li>
       <li style={li_top_style}>
         TODO (short-term)
         <ul style={ul_style}>
-          <li style={li_style}>LOTS of simple styling/UI improvements</li>
           <li style={li_style}>Group local devices by MAC Address</li>
           <li style={li_style}>Gather local info from mDNS/Bonjour</li>
         </ul>
