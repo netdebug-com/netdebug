fb6e5fdf6cb1c78a4b224d1aa4c133b963dfe222
Rob Sherwood <rob.sherwood@gmail.com>
2024-04-02 15:19:37 -0700

    Update the copy binarires for win32 + latest symlink
     ### Summary:
    See title, but "works for me"
     ### Test Plan:
    MacOS *untested* need Gregor to test that

--- scripts/copy-desktop-binaries.sh
+++ scripts/copy-desktop-binaries.sh
@@ -3,7 +3,6 @@
 set -e
 set -x 
 
-test -d frontend/electron/out/make/zip
 
 destinations="dal1"
 os=$(uname)
@@ -11,10 +10,25 @@ os=$(uname)
 
 echo $os
 if test "$os" == "Darwin"; then
+    test -d frontend/electron/out/make/zip
+    dmg=`cd frontend/electron/out/make/zip/ && ls *.dmg`
     for dest in $destinations; do
         scp frontend/electron/out/make/zip/darwin/universal/*.zip root@${dest}.topology.netdebug.com:updates_dir/darwin/universal/
         scp frontend/electron/out/make/zip/darwin/universal/RELEASES.json root@${dest}.topology.netdebug.com:updates_dir/darwin/universal/
         scp frontend/electron/out/make/*.dmg root@${dest}.topology.netdebug.com:updates_dir/darwin/universal/
+	# create latest symlink
+	ssh root@${dest}.topology.netdebug.com "cd updates_dir/darwin/universal/ && ln -s ${dmg} desktop-latest.zip" 
+    done
+elif test "$os" == MINGW64_NT-10.0-22631 ; then 
+    win_out=frontend/electron/out/make/squirrel.windows/x64
+    test -d $win_out
+    exe=`cd $win_out && ls *.exe`
+    for dest in $destinations ; do
+	# NOTE: the .exe is for the initial install and the .nupkg is for upgrading existing clients
+	scp $win_out/*.nupkg $win_out/*.exe root@${dest}.topology.netdebug.com:updates_dir/win32/x64/
+	scp $win_out/RELEASES root@${dest}.topology.netdebug.com:updates_dir/win32/x64/
+	# create latest symlink
+	ssh root@${dest}.topology.netdebug.com "cd updates_dir/win32/x64/ && ln -s \"${exe}\" desktop-latest.exe" 
     done
 fi
 
