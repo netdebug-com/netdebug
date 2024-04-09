40db99389ed025beef76925fe5e65ad6b6e02719
Gregor Maier <gregor.maier@netdebug.com>
2024-04-09 12:51:29 -0400

    UI: better error element
     ### Summary:
     Sometime the error element gets an `ErrorResponse` instance from
     react-router but so far we've only expected regular js Error
     instances.
    
     ### Test Plan:
     copied this change to console.netdebug.com: observed a more legible
     error message

--- frontend/common/src/components/DefaultErrorElement.tsx
+++ frontend/common/src/components/DefaultErrorElement.tsx
@@ -1,10 +1,25 @@
-import { useRouteError } from "react-router";
+import { isRouteErrorResponse, useRouteError } from "react-router";
 import { ErrorMessage } from "./ErrorMessage";
 
 export function DefaultErrorElement() {
   // useRouteError() will return whatever a load has thrown. Since JS allows one to
-  // throw anything, not just `Error` instance we could in theory get something other
-  // than an error instance. But lets ignore that for now
-  const error = useRouteError() as Error;
-  return <ErrorMessage msg={error.name + ": " + error.message} />;
+  // throw anything, not just `Error`. In particular, react-router will/can return an
+  // `ErrorResponse` which essentially wraps a `fetch()` resposne but with the body resolved.
+  // TODO: find out if we should use ErrorReponse for errors from our loaders
+  const error = useRouteError();
+  if (isRouteErrorResponse(error)) {
+    // is this an ErrorResponse instance?
+    let msg = error.status + ": " + error.statusText;
+    if (error.data) {
+      msg += error.data;
+    }
+    return <ErrorMessage msg={msg} />;
+  } else if (error instanceof Error) {
+    // A JS `Error`
+    return <ErrorMessage msg={error.name + ": " + error.message} />;
+  } else {
+    // Who knows what's going on
+    console.log("DefaultErrorElement", error);
+    return <ErrorMessage msg={"Unknown Error"} />;
+  }
 }
