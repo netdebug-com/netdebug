835f44b195c7ded33cb84c85f2a9fc8b61e69529
Rob Sherwood <135524607+capveg-netdebug@users.noreply.github.com>
2024-04-24 15:59:43 -0700

    Auto-register devices on connect if not in the device table (#804)
    ### Summary:
    
    Currently everything gets put into the beta program (orgid=1) and
    there's no auth, but at least this way everything shows up in
    the default devices view.
    
     ### Test Plan:
    
    Added a new test

--- webserver/tests/test_devices.rs
+++ webserver/tests/test_devices.rs
@@ -1,9 +1,13 @@
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
 use axum::http::StatusCode;
 #[cfg(test)]
 use common::init::netdebug_test_init;
 use gui_types::{PublicDeviceDetails, PublicDeviceInfo};
 use http_body_util::BodyExt;
-use libwebserver::remotedb_client::RemoteDBClient;
+use libwebserver::{organizations::NETDEBUG_EMPLOYEE_ORG_ID, remotedb_client::RemoteDBClient};
+use tokio_postgres::Client;
+use uuid::Uuid;
 
 use crate::db_utils::{
     add_fake_connection_logs, add_fake_devices, add_fake_users, get_auth_token_from_rest_router,
@@ -110,4 +114,57 @@ async fn test_devices_details() {
     // two flows stored
     assert_eq!(details.num_flows_stored, 2);
     assert_eq!(details.num_flows_with_send_loss, Some(1));
+}
+
+async fn count_devices(db_client: &Client) -> Result<i64, tokio_postgres::error::Error> {
+    let row = db_client
+        .query_one("SELECT COUNT(*) FROM devices", &[])
+        .await?;
+    row.try_get::<_, i64>(0)
+}
+
+#[tokio::test]
+async fn test_devices_auto_registration() {
+    // TODO: create our own test fixture to setup all of this for us...
+    netdebug_test_init();
+    let (db_client, test_db) = mk_test_db("devices_netdebug_auto_registration")
+        .await
+        .unwrap();
+    let remotedb_client = RemoteDBClient::mk_mock(&test_db.db_uri);
+    remotedb_client
+        .create_table_schema(&db_client)
+        .await
+        .unwrap();
+    add_fake_users(&db_client).await;
+    add_fake_devices(&db_client).await;
+    add_fake_connection_logs(&db_client).await.unwrap();
+
+    let device_uuid = Uuid::new_v4(); // random UUID
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(128, 8, 128, 38)), 8080);
+
+    let initial_device_count = count_devices(&db_client).await.unwrap();
+    // register a new device
+    RemoteDBClient::handle_log_device_connect(
+        &db_client,
+        &NETDEBUG_EMPLOYEE_ORG_ID,
+        &device_uuid,
+        "test",
+        &addr,
+    )
+    .await
+    .unwrap();
+    let after_device_count = count_devices(&db_client).await.unwrap();
+    assert_eq!(after_device_count, initial_device_count + 1);
+    // try to register it again and make sure it's a NOOP
+    RemoteDBClient::handle_log_device_connect(
+        &db_client,
+        &NETDEBUG_EMPLOYEE_ORG_ID,
+        &device_uuid,
+        "test",
+        &addr,
+    )
+    .await
+    .unwrap();
+    let noop_device_count = count_devices(&db_client).await.unwrap();
+    assert_eq!(noop_device_count, after_device_count);
 }
