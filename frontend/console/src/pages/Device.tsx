3095cb3ed8349601658e39ed0c59f98ac9017eb0
Rob Sherwood <135524607+capveg-netdebug@users.noreply.github.com>
2024-05-07 12:16:56 -0700

    Initial pass on first-hop router latency graph (#812)
    ### Summary:
    
    Graphs p50 and p99 latency for a single device in the per-device view.
    
    The complete graph has rendering performance issues which I've tracked
    down to apexcharts.  To just brute force work around them, I've added a
    "LIMIT 2000" to the SQL query so we only show the last 2000 most recent
    data points.  Seems ok for now.
    
     ### Test Plan:
    
    Lots of manual t
    
    ![Screenshot 2024-05-01 154522.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/Se6cfQDK8krglOiJ7Bgl/6d62b4ce-cb2b-403b-88db-dcb0dd1c2560.png)
    
    esting

--- frontend/console/src/pages/Device.tsx
+++ frontend/console/src/pages/Device.tsx
@@ -15,6 +15,7 @@ import {
   renderDataLoadingState,
 } from "../common";
 import { useEffect, useState } from "react";
+import ReactApexChart from "react-apexcharts";
 
 interface DeviceLoaderArgs extends ActionFunctionArgs {
   params: Params<ParamParseKey<string>>;
@@ -40,20 +41,185 @@ function renderDeviceDetails(
   );
 }
 
+function generateTimeSeriesChartOptions(): ApexCharts.ApexOptions {
+  return {
+    chart: {
+      type: "area",
+      stacked: false,
+      height: 350,
+      zoom: {
+        type: "x",
+        enabled: true, // this is what allows autozooming of time
+        autoScaleYaxis: true,
+      },
+      toolbar: {
+        autoSelected: "zoom",
+      },
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    markers: {
+      size: 0,
+    },
+    title: {
+      text: "Latency to First-Hop Router",
+      align: "left",
+    },
+    fill: {
+      type: "gradient",
+      gradient: {
+        shadeIntensity: 1,
+        inverseColors: false,
+        opacityFrom: 0.5,
+        opacityTo: 0,
+        stops: [0, 90, 100],
+      },
+    },
+    yaxis: {
+      labels: {
+        formatter: function (val) {
+          return (val / 1000000).toFixed(0); // map ns to ms
+        },
+      },
+      title: {
+        text: "Latency (milliseconds)",
+      },
+    },
+    xaxis: {
+      type: "datetime", // I think this is the magic that says interpret the 'x' value as seconds from the EPOCH
+      // ApexCharts documentation isn't super clear here
+    },
+    tooltip: {
+      shared: false,
+      y: {
+        formatter: function (val) {
+          return (val / 1000000).toFixed(2); // map ns to ms for the tooltip
+        },
+      },
+    },
+  };
+}
+
+interface PingDataPoint {
+  x: string;
+  y: number;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function generateTimeSeriesChartDataMerged(
+  firstHopTimeSeries: DataLoadingState<Array<Array<FirstHopTimeSeriesData>>>,
+): ApexAxisChartSeries | ApexNonAxisChartSeries {
+  // TODO: outer array for p50, p99, etc.
+  if (!firstHopTimeSeries.data) {
+    return []; // still loading or error
+  }
+  // else data is loaded
+  const p50_map = new Map<string, Array<PingDataPoint>>();
+  firstHopTimeSeries.data.forEach((intf: Array<FirstHopTimeSeriesData>) => {
+    const series_data = intf.map((ping_data: FirstHopTimeSeriesData) => ({
+      x: ping_data.time,
+      y: ping_data.aggregate_ping_data.rtt_p50_ns,
+    }));
+    const name = intf[0].interface_name + " p50";
+    if (p50_map.has(name)) {
+      p50_map.get(name).push(...series_data);
+    } else {
+      p50_map.set(name, series_data);
+    }
+  });
+  const p99_map = new Map<string, Array<PingDataPoint>>();
+  firstHopTimeSeries.data.forEach((intf: Array<FirstHopTimeSeriesData>) => {
+    const series_data = intf.map((ping_data: FirstHopTimeSeriesData) => ({
+      x: ping_data.time,
+      y: ping_data.aggregate_ping_data.rtt_p99_ns,
+    }));
+    const name = intf[0].interface_name + " p99";
+    if (p99_map.has(name)) {
+      p99_map.get(name).push(...series_data);
+    } else {
+      p99_map.set(name, series_data);
+    }
+  });
+  const p50_series = Array.from(
+    p50_map,
+    ([k, v]: [string, Array<PingDataPoint>]) => ({ name: k, data: v }),
+  );
+  const p99_series = Array.from(
+    p99_map,
+    ([k, v]: [string, Array<PingDataPoint>]) => ({ name: k, data: v }),
+  );
+  return [p50_series, p99_series].flat();
+}
+
+function generateTimeSeriesChartData(
+  firstHopTimeSeries: DataLoadingState<Array<Array<FirstHopTimeSeriesData>>>,
+): ApexAxisChartSeries | ApexNonAxisChartSeries {
+  // TODO: outer array for p50, p99, etc.
+  if (!firstHopTimeSeries.data) {
+    return []; // still loading or error
+  }
+  // else data is loaded
+  const p50_series = firstHopTimeSeries.data.map(
+    (intf: Array<FirstHopTimeSeriesData>) => {
+      const series_data = intf.map((ping_data: FirstHopTimeSeriesData) => ({
+        x: ping_data.time,
+        y: ping_data.aggregate_ping_data.rtt_p50_ns,
+      }));
+      return {
+        name: intf[0].interface_name + " p50",
+        data: series_data,
+      };
+    },
+  );
+  const p99_series = firstHopTimeSeries.data.map(
+    (intf: Array<FirstHopTimeSeriesData>) => {
+      const series_data = intf.map((ping_data: FirstHopTimeSeriesData) => ({
+        x: ping_data.time,
+        y: ping_data.aggregate_ping_data.rtt_p99_ns,
+      }));
+      return {
+        name: intf[0].interface_name + " p99",
+        data: series_data,
+      };
+    },
+  );
+  return [p50_series, p99_series].flat();
+}
+
 function renderFirstHopTimeSeries(
   uuid: string,
-  firstHopTimeSeries: DataLoadingState<
-    Map<string, Array<FirstHopTimeSeriesData>>
-  >,
+  firstHopTimeSeries: DataLoadingState<Array<Array<FirstHopTimeSeriesData>>>,
 ): JSX.Element {
-  console.log("TimeSeries ", firstHopTimeSeries);
+  const time_series_data = generateTimeSeriesChartData(firstHopTimeSeries);
+  console.log(
+    "TimeSeries Data : #labels=",
+    time_series_data.map((d) => d.name),
+  );
+  let count = 0;
+  time_series_data.forEach((d) => {
+    count += d.data.length;
+    console.log(
+      "TimeSeries Data: label " + d.name + " :: points=" + d.data.length,
+    );
+  });
+  console.log("Time Series total data points graphed: " + count);
   return (
-    <details>
-      <summary>First-Router Time Series for {uuid} </summary>
-      {renderDataLoadingState(firstHopTimeSeries, (d) => (
-        <pre>{JSON.stringify(d, null, 2)}</pre>
-      ))}
-    </details>
+    <div>
+      <ReactApexChart
+        options={generateTimeSeriesChartOptions()}
+        series={time_series_data}
+        type="area"
+        height={350}
+      />
+
+      <details>
+        <summary>First-Router Time Series for {uuid} </summary>
+        {renderDataLoadingState(firstHopTimeSeries, (d) => (
+          <pre>{JSON.stringify(d, null, 2)}</pre>
+        ))}
+      </details>
+    </div>
   );
 }
 
@@ -67,7 +233,7 @@ export function Device() {
   }, [uuid]);
 
   const [firstHopTimeSeries, setFirstHopTimeSeries] = useState(
-    new DataLoadingState<Map<string, Array<FirstHopTimeSeriesData>>>(),
+    new DataLoadingState<Array<Array<FirstHopTimeSeriesData>>>(),
   );
   useEffect(() => {
     loadDataWithAuth(
